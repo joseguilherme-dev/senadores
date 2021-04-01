fa50031344c53964cbc7cf14aa090756d912d927
José Guilherme <joseguilhermefmoura@gmail.com>
2021-04-01 02:04:18 -0300

    :arrow_up: Update settings.py

--- senadores/settings.py
+++ senadores/settings.py
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'senadores.pipelines.SenadoresPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'senadores.pipelines.MongoPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -86,3 +86,6 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MONGO_URI = 'mongodb://127.0.0.1'
+MONGO_DATABASE = 'senators'
