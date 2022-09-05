86db51e7b0dc872a6be653c42ed4b3bd631026e0
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-05 18:11:31 -0300

    :wrench: Moving files

---
+++ senators/spiders/senators.py
@@ -0,0 +1,52 @@
+import scrapy
+from scrapy.spiders import CrawlSpider
+from ..items import SenadoresItem
+
+class SenatorsSpider(CrawlSpider):
+    name = 'senators_spider'
+    BASE_URL = 'https://www25.senado.leg.br/web/senadores/em-exercicio/-/e/por-nome'
+    mongo_collection = 'senators'
+    duplicity_condition = ['url']
+
+    def __init__(self):
+        pass
+
+    # Essa função deve sempre estar presente, é um gerador que deve ser o primeiro a ser chamado.
+    def start_requests(self):
+
+        yield scrapy.Request(url=self.BASE_URL, callback=self.parse_senators_list)
+
+    def parse_senators_list(self, response):
+        
+        # Recebe o elemento do tipo iterativo Selector
+        senators_list = response.xpath('//*[contains(@id,"senadoresemexercicio-tabela-senadores")]/tbody/tr')
+
+    
+        for senator in senators_list:
+            meta = { 
+                'senator_url' : senator.xpath('td[1]/a/@href').extract_first(),
+                'senator_name' : senator.xpath('td[1]/a/text()').extract_first(),
+                'senator_party' : senator.xpath('td[2]//text()').extract_first(),
+                'senator_uf' : senator.xpath('td[3]//text()').extract_first(),
+                'senator_period' : senator.xpath('td[4]//text()').extract_first(),
+                'senator_phones' : senator.xpath('td[5]//text()').extract_first(),
+                'senator_emails' : senator.xpath('td[6]//text()').extract_first()
+            }
+
+            yield scrapy.Request(url=meta.get('senator_url'), meta=meta, callback=self.parse_senator_page)
+
+        return 0
+
+    def parse_senator_page(self, response):
+        senator = SenadoresItem()
+
+        senator['url'] = response.meta.get('senator_url')
+        senator['name']  = response.meta.get('senator_name')
+        senator['party'] = response.meta.get('senator_party')
+        senator['uf'] = response.meta.get('senator_uf')
+        senator['period'] = response.meta.get('senator_period')
+        senator['phones'] = response.meta.get('senator_phones')
+        senator['email'] = response.meta.get('senator_emails')
+        senator['address'] = response.xpath('//div[contains(@class,"dadosPessoais")]/dl/dd[4]//text()').extract_first().strip()
+
+        yield senator
